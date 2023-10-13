3870ba9f9e3b8600de5212cdd271ad9bb0bd308e
serhii <tnmbel@gmail.com>
2023-10-13 20:12:24 +0100

    add comments

--- Boot Camp/Project SQL Data Cleaning/data_cleaning.sql
+++ Boot Camp/Project SQL Data Cleaning/data_cleaning.sql
@@ -1,16 +1,10 @@
-/*
 
-Cleaning Data in SQL Queries
+-- CLEANING DATA IN SQL QUERIES
 
-*/
+SELECT *
+FROM PortfolioProject.NashvilleHousing;
 
-
-Select *
-From PortfolioProject.NashvilleHousing;
-
--- --------------------------------------------------------------------------------------------------------------------------
-
--- Standardize Date Format
+-- STANDARTIZE DATA FORMAT
 
 UPDATE PortfolioProject.NashvilleHousing
 SET SaleDate = STR_TO_DATE(SaleDate, '%M %e, %Y');
@@ -19,17 +13,18 @@ ALTER TABLE PortfolioProject.NashvilleHousing
     MODIFY COLUMN SaleDate DATE;
 
 
--- Populate Property Address data
+-- POPULATE PROPERTY ADDRESS DATA
 
 -- Replace empty values with NULL
 UPDATE PortfolioProject.NashvilleHousing
 SET
     PropertyAddress = CASE WHEN PropertyAddress = '' THEN NULL ELSE PropertyAddress END;
 
-Select *
-From PortfolioProject.NashvilleHousing
-     Where PropertyAddress is null
-order by ParcelID;
+-- Visual check of changes made to the table.
+SELECT *
+FROM PortfolioProject.NashvilleHousing
+     WHERE PropertyAddress IS NULL
+ORDER BY ParcelID;
 
 -- Retrieve PropertyAddress values from a self-join of NashvilleHousing where PropertyAddress is null in 'a' and fallback to 'b'
 SELECT a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, IFNULL(a.PropertyAddress, b.PropertyAddress)
@@ -45,7 +40,7 @@ SET a.PropertyAddress = IFNULL(a.PropertyAddress, b.PropertyAddress)
 WHERE a.PropertyAddress IS NULL;
 
 
--- Breaking out Address into Individual Columns (Address, City, State)
+-- BREAKING OUT ADDRESS INTO INDIVIDUAL COLUMNS (Address, City, State)
 
 Select PropertyAddress
 From PortfolioProject.NashvilleHousing;
@@ -66,34 +61,47 @@ SELECT
 FROM PortfolioProject.NashvilleHousing;
 
 
--- Add column PropertySplitAddress
+-- ADD COLUMN PropertySplitAddress
+
+-- Add Property Split Address Column: Adds a new column 'PropertySplitAddress'.
+
 ALTER TABLE PortfolioProject.NashvilleHousing
     Add PropertySplitAddress VARCHAR(255);
 
+-- Update Property Split Address: Populates the 'PropertySplitAddress' column with the part of 'PropertyAddress' before the comma (',').
 Update PortfolioProject.NashvilleHousing
 SET PropertySplitAddress =  SUBSTRING(PropertyAddress, 1, LOCATE(',', PropertyAddress) - 1);
 
+-- Add Property Split City Column: Adds a new column 'PropertySplitCity'
 
 ALTER TABLE PortfolioProject.NashvilleHousing
     Add PropertySplitCity VARCHAR(255);
 
+-- Update Property Split City: Populates the 'PropertySplitCity' column with the part of 'PropertyAddress' after the comma (',').
+
 Update PortfolioProject.NashvilleHousing
 SET PropertySplitCity = SUBSTRING(PropertyAddress, LOCATE(',', PropertyAddress) + 1, CHAR_LENGTH(PropertyAddress));
 
+-- Visual check of changes made to the table NashvilleHousing.
 
 Select PropertyAddress,PropertySplitCity, PropertySplitAddress
 From PortfolioProject.NashvilleHousing;
 
 
 -- Owner Adress
+
+-- Update Owner Address: Set empty 'OwnerAddress' values to NULL.
+
 UPDATE PortfolioProject.NashvilleHousing
 SET
     OwnerAddress = CASE WHEN OwnerAddress = '' THEN NULL ELSE OwnerAddress END;
+-- Select Null Owner Addresses: Retrieve records where 'OwnerAddress' is NULL.
 
-Select OwnerAddress
-From PortfolioProject.NashvilleHousing
-WHERE OwnerAddress is null;
+SELECT OwnerAddress
+FROM PortfolioProject.NashvilleHousing
+WHERE OwnerAddress IS NULL ;
 
+-- Split Owner Address: Extract and split 'OwnerAddress' into three parts, Part1, Part2, and Part3.
 
 SELECT OwnerAddress,
     SUBSTRING_INDEX(SUBSTRING_INDEX(REPLACE(OwnerAddress, ',', '.'), '.', -3), '.', 1) AS Part1,
@@ -101,67 +109,68 @@ SELECT OwnerAddress,
     SUBSTRING_INDEX(SUBSTRING_INDEX(REPLACE(OwnerAddress, ',', '.'), '.', -1), '.', 1) AS Part3
 FROM PortfolioProject.NashvilleHousing;
 
-
-
+-- Add Owner Split Address Column: Add a new column 'OwnerSplitAddress' to the table.
 
 ALTER TABLE PortfolioProject.NashvilleHousing
-    Add OwnerSplitAddress VARCHAR(255);
+    ADD OwnerSplitAddress VARCHAR(255);
+-- Update Owner Split Address: Populate 'OwnerSplitAddress' with the first part of the split 'OwnerAddress'.
 
-Update PortfolioProject.NashvilleHousing
+UPDATE PortfolioProject.NashvilleHousing
 SET OwnerSplitAddress =SUBSTRING_INDEX(SUBSTRING_INDEX(REPLACE(OwnerAddress, ',', '.'), '.', -3), '.', 1);
 
+-- Add Owner Split City Column: Add a new column 'OwnerSplitCity' to the table.
 
 ALTER TABLE PortfolioProject.NashvilleHousing
-    Add OwnerSplitCity VARCHAR(255);
+    ADD OwnerSplitCity VARCHAR(255);
 
-Update PortfolioProject.NashvilleHousing
+-- Update Owner Split City: Populate 'OwnerSplitCity' with the second part of the split 'OwnerAddress'.
+
+UPDATE PortfolioProject.NashvilleHousing
 SET OwnerSplitCity = SUBSTRING_INDEX(SUBSTRING_INDEX(REPLACE(OwnerAddress, ',', '.'), '.', -2), '.', 1);
 
-
+-- Add Owner Split State Column: Add a new column 'OwnerSplitState' to the table.
 
 ALTER TABLE PortfolioProject.NashvilleHousing
-    Add OwnerSplitState VARCHAR(255);
+    ADD OwnerSplitState VARCHAR(255);
 
-Update PortfolioProject.NashvilleHousing
+-- Update Owner Split State: Populate 'OwnerSplitState' with the third part of the split 'OwnerAddress'.
+
+UPDATE PortfolioProject.NashvilleHousing
 SET OwnerSplitState = SUBSTRING_INDEX(SUBSTRING_INDEX(REPLACE(OwnerAddress, ',', '.'), '.', -1), '.', 1);
-
-
-
-Select *
-From PortfolioProject.NashvilleHousing;
-
-
-
-
-# --------------------------------------------------------------------------------------------------------------------------
 
 
 -- Change Y and N to Yes and No in "Sold as Vacant" field
 
-
-Select Distinct(SoldAsVacant), Count(SoldAsVacant)
-From PortfolioProject.NashvilleHousing
-Group by SoldAsVacant
-order by 2;
-
-
+-- Count occurrences of distinct values in 'SoldAsVacant' and sort by count.
+SELECT DISTINCT (SoldAsVacant), Count(SoldAsVacant)
+FROM PortfolioProject.NashvilleHousing
+GROUP BY SoldAsVacant
+ORDER BY 2;
 
 
-Select SoldAsVacant
-     , CASE When SoldAsVacant = 'Y' THEN 'Yes'
-            When SoldAsVacant = 'N' THEN 'No'
+/*
+This code checks a condition before making changes in the database and selects the "SoldAsVacant" column,
+replacing 'Y' with 'Yes', 'N' with 'No', and keeping the original value for any other cases in the NashvilleHousing table
+of the PortfolioProject database.
+ */
+
+SELECT SoldAsVacant
+     , CASE WHEN SoldAsVacant = 'Y' THEN 'Yes'
+            WHEN SoldAsVacant = 'N' THEN 'No'
             ELSE SoldAsVacant
     END
-From PortfolioProject.NashvilleHousing;
+FROM PortfolioProject.NashvilleHousing;
 
+/*
+This is a SQL query that updates a column called 'SoldAsVacant' in a database table called 'NashvilleHousing'.
+The query uses a CASE statement to set the value of 'SoldAsVacant' to 'Yes' or 'No' depending on its current value.
+ */
 
-Update PortfolioProject.NashvilleHousing
-SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
-                        When SoldAsVacant = 'N' THEN 'No'
+UPDATE PortfolioProject.NashvilleHousing
+SET SoldAsVacant = CASE WHEN SoldAsVacant = 'Y' THEN 'Yes'
+                        WHEN SoldAsVacant = 'N' THEN 'No'
                         ELSE SoldAsVacant
                        END
-
-#     -----------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Remove Duplicates
 
@@ -187,12 +196,11 @@ Select *
 From PortfolioProject.NashvilleHousing;
 
 
-#  ---------------------------------------------------------------------------------------------------------
 
 -- Delete Unused Columns
 
-Select *
-From PortfolioProject.NashvilleHousing;
+SELECT *
+FROM PortfolioProject.NashvilleHousing;
 
 
 ALTER TABLE PortfolioProject.NashvilleHousing
